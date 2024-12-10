a96cd1534288e7335fb8b846e3d5ee551583c9a8
Agustin-Villalba-Guillerat <83144805+Agustin-Villalba-Guillerat@users.noreply.github.com>
2024-12-10 15:34:45 -0300

    Add files via upload

---
+++ tpfinalparte2.js
@@ -0,0 +1,17 @@
+let pantallas;
+let sonido;
+
+function preload() {
+    sonido = loadSound("data/musica_fondo.mp3");
+}
+
+function setup() {
+    createCanvas(640, 480);
+    pantallas = new Pantallas();
+}
+
+
+function draw() {
+   
+    pantallas.dibujar();
+}
